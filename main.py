2f965edabefcc4647130fbdeda2b26ae94784f33
antoniospadea <coga.antonio@hotmail.it>
2022-11-29 02:45:38 +0100

    Funzione di conversione senza nessun controllo

--- main.py
+++ main.py
@@ -6,4 +6,11 @@ def convert_roman_to_arabic(roman: str) -> str:
     :return: converted string
     """
     # inserisci qui il tuo codice
-    pass
+    Numero = 0
+    Somma = 0
+    Roman_Number_Dict = {'I': 1,'V': 5,'X': 10, 'L': 50,'C': 100,'D': 500,'M': 1000}
+    for i in range(len(roman)):
+        Numero = Roman_Number_Dict[roman[i]]
+        Somma = Somma + Numero
+    Risultato = str(Somma)
+    return Risultato
