298967cb0186695dec00514f35b85d8cbc6d352e
bigbigbig0 <angelospadea@hotmail.it>
2022-12-01 00:41:22 +0100

    Implementata la funzione che converte un numero romano in uno arabo

--- main.py
+++ main.py
@@ -8,14 +8,22 @@ def convert_roman_to_arabic(roman: str) -> str:
     
     #creo un dizionario contenente i valori associati ai numeri romani
     dict={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
-    c=0;
+    #controllo che l'ultimo valore sia un valore valido se lo è lo uso come valore di partenza
+    if dict.get(roman[len(roman)-1],-1)==-1:
+        raise ValueError 
+    else:
+        c=dict.get(roman[len(roman)-1])
     
-    for i in range(len(roman)):
+    for i in range(len(roman)-1):
         #controllo che ogni elemento della stringa sia valido
         b=dict.get(roman[i],-1)
-        if b==-1:
-            raise ValueError        
+        h=dict.get(roman[i+1],-1)
+        if b==-1 or h==-1:
+            raise ValueError     
         else:
-            #somma il valore della stringa con quello precedente
-            c+=b
+            #se il primo valore (b) è maggiore del secondo (h) allora li sommo altrimenti lo sottraggo
+            if b>=h:
+                c+=b
+            else:
+                c-=b    
     return str(c)
