7bc9e0c1b0653f23d1f2df73a99d0b3e9fd8c6c9
bigbigbig0 <angelospadea@hotmail.it>
2022-11-30 20:50:37 +0100

    Se un valore non è ammissibile restituisce errore

--- main.py
+++ main.py
@@ -5,5 +5,14 @@ def convert_roman_to_arabic(roman: str) -> str:
     :param roman: string to be converted
     :return: converted string
     """
-    # inserisci qui il tuo codice
-    pass
+    
+    #creo un dizionario contenente i valori associati ai numeri romani
+    dict={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+    c=0;
+    
+    for i in range(len(roman)):
+        #controllo che ogni elemento della stringa sia valido
+        b=dict.get(roman[i],-1)
+        if b==-1:
+            raise ValueError        
+    return str(c)
