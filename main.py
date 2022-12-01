05444df46cc66cdcd6c09607c0f49855d239c37c
antoniospadea <coga.antonio@hotmail.it>
2022-12-01 03:33:43 +0100

    Aggiunta del controlo del precedente Controlliamo che il successivo sia minore del valore corrente in caso favorevole si continua con la somma in caso in caso contrario si va ad effettuare la sottrazione. In fine si somma lultimo valore che non viene confrontanto con nessuno

--- main.py
+++ main.py
@@ -20,11 +20,19 @@ def convert_roman_to_arabic(roman: str) -> str:
     Numero = 0
     Somma = 0
     Roman_Number_Dict = {'I': 1,'V': 5,'X': 10, 'L': 50,'C': 100,'D': 500,'M': 1000}
-    for i in range(len(roman)):
-        if roman[i] in Roman_Number_Dict:
-            Numero = Roman_Number_Dict[roman[i]]
-            Somma = Somma + Numero
+    for i in range(len(roman)-1):
+        if (roman[i] in Roman_Number_Dict) and (roman[i+1] in Roman_Number_Dict):
+            Numero1 = Roman_Number_Dict[roman[i]]
+            Numero2 = Roman_Number_Dict[roman[i+1]]
+            if Numero1 >= Numero2:
+                Somma = Somma + Numero1
+            else:
+                Somma = Somma - Numero1
         else:
             raise ValueError
+    if roman[len(roman)-1] in Roman_Number_Dict:
+        Somma = Somma + Roman_Number_Dict[roman[len(roman)-1]] 
+    else:
+        raise ValueError
     Risultato = str(Somma)
     return Risultato
