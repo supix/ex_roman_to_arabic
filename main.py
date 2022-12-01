5762b3b208fd799b9741b0780c64848c9b64a9c6
antoniospadea <coga.antonio@hotmail.it>
2022-12-01 02:35:47 +0100

    Revert "aggiunta controllo per lindividuazione di"
    This reverts commit 1a7f66c657fa10ac8715ad3c88b22791e717fbd3.

--- main.py
+++ main.py
@@ -6,25 +6,11 @@ def convert_roman_to_arabic(roman: str) -> str:
     :return: converted string
     """
     # inserisci qui il tuo codice
-    """_summary_
-
-    Raises:
-        ValueError: Quando nel parametro @roman viene inserito
-        un carattere diverso da I , V , X , L , C , D , M 
-        restituisce un errore
-
-    Returns:
-        _type_: la funzione restituisce la somma dei @Numeri 
-        che sono convertiti tramite il dizionario @Roman_Number_Dict
-    """
     Numero = 0
     Somma = 0
     Roman_Number_Dict = {'I': 1,'V': 5,'X': 10, 'L': 50,'C': 100,'D': 500,'M': 1000}
     for i in range(len(roman)):
-        if roman[i] in Roman_Number_Dict:
-            Numero = Roman_Number_Dict[roman[i]]
-            Somma = Somma + Numero
-        else:
-            raise ValueError
+        Numero = Roman_Number_Dict[roman[i]]
+        Somma = Somma + Numero
     Risultato = str(Somma)
     return Risultato
